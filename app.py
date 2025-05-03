4ee76865111366ad709cacb0db09db3b27ca6cb8
a <a@as-MacBook-Pro.local>
2025-05-03 20:29:46 +0530

    initial

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def download(format):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
